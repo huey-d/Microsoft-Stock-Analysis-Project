4d4507ebe87cc9f54876d363f6476021030fd0e8
yo-huey <huydn07@gmail.com>
2023-02-27 01:51:18 -0800

    building fucntions to create stock market data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,19 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import yfinance as yf
+import yahoofinancials
 
+def stock_price_dataset(company: str, period: str = 'max', interval: str = '1d'):
+    
+    
+    stock = yf.Ticker(company)
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    stock_data = stock.history(period = period, interval = interval)
+    
+    print(stock_data)
+    pass
 
+def company_dataset():
+    pass
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    stock_price_dataset('AAPL')
